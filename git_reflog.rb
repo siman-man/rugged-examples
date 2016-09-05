0e0dfdc952e3be1171e5ff4559ca6343b15cf143
siman-man <k128585@ie.u-ryukyu.ac.jp>
2016-09-05 23:37:55 +0900

    Fix command

--- git_reflog.rb
+++ git_reflog.rb
@@ -5,8 +5,7 @@ repo = Rugged::Repository.new('.')
 
 ref = repo.head
 
-repo.walk(repo.last_commit).reverse_each do |entry|
-#ref.log.reverse_each do |entry|
+ref.log.reverse_each do |entry|
   sha = entry[:id_new]
   message = entry[:message]
   committer = entry[:committer]
